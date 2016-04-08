7ba1c36117f514934d6f46e60cd36a84dccaf5be
Antonio Grimaldo <grimi94@gmail.com>
2016-04-08 08:08:24 -0500

    Feat: plot now compares grammys vs charts

--- process.R
+++ process.R
@@ -1,12 +1,9 @@
 library("ggplot2")
-charts_clean = read.delim("./charts_clean.tsv")
 
-# scatter plot settings
-scatter_plot <- ggplot(charts_clean, aes(x=Year, y=Time.num)) + 
-  geom_point(alpha=1/80, color="blue") + 
-  geom_smooth(color="Black") + 
-  labs(x="Year", y="Length (seconds)", title="Year vs Songs Length") 
-print(scatter_plot)
+charts_clean = read.delim("./charts_clean.tsv")
+grammys = read.delim("./grammys.tsv", header=FALSE)
+colnames(grammys) <- c("Year","Track")
+
 
 # mode function, source: stackoverflow
 Mode <- function(x) {
@@ -29,4 +26,26 @@ histogram_plot <-  ggplot(charts_clean, aes(Time.num), sd = sd(y)) + geom_histog
   #geom_vline(aes(xintercept = mode), color="#FF9999") +
   labs(x="Song Length(seconds)", title="Songs Length Histogram") 
 
-print(histogram_plot)
+#print(histogram_plot)
+func_setSongLength <- function(trackName){
+  result <- charts_clean[charts_clean[, "Track"] == as.character(trackName),,drop=FALSE]
+  value <- head(result,1)$Time.num
+  if(length(value) > 0){
+    return(value)
+  } else {
+    return(NA)
+  }
+}
+
+grammys$Time.num <- sapply(grammys$Track, func_setSongLength)
+
+is.na(grammys$Time.num) <- which(grammys$Time.num == "")
+grammys <- subset(grammys, !is.na(grammys$Time.num))
+# scatter plot settings
+
+
+scatter_plot <- ggplot() + 
+  geom_smooth(data=grammys, aes(x=Year, y=Time.num), color="Black") + 
+  geom_smooth(data=charts_clean, aes(x=Year, y=Time.num), color="Blue") + 
+  labs(x="Year", y="Length (seconds)", title="Year vs Songs Length") +
+print(scatter_plot)
